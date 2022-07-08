2993d863fec4ecee776b1641d743d595f7f392ca
Vrl_mck <mackofee-328@yandex.com>
2022-07-08 18:58:52 +0300

    Update get_weather.h

--- get_weather.h
+++ get_weather.h
@@ -12,6 +12,6 @@ float get_weather(string city)
 {
         string link = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=API_KEY";
         auto js_obj_2 = nlohmann::json::parse(get_request(link));
-        return js_obj_2["weather.main"].get<float>();
+        return js_obj_2["weather.main"].get<float>()-273;// -273 in celcius
 }
 #endif
